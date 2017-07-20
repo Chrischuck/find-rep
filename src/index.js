567303bc8362b316de8d28fb6f2e9fbb3706f4be
Chris <chrischuck35@gmail.com>
2017-07-19 22:54:35 -0700

    Add's basic start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { h, render } from 'preact';
+
+render((
+	<div id="foo">
+		<span>Hello, world!</span>
+		<button onClick={ e => alert("hi!") }>Click Me</button>
+	</div>
+), document.getElementById('app'));
